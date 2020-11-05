4a1b332491e527e9a8b2b62056245073a5f8851e
Binit Kumar <bkumar@rocketsoftware.com>
2020-11-05 15:04:52 +0530

    Crates.io related changes

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-    println!("cargo:rustc-link-search=<path to CLI Driver Bin>\\clidriver\\bin"); // the "-L" flag
+    println!("cargo:rustc-link-search=C:\\Personal\\R\\clidriver\\bin"); // the "-L" flag
     println!("cargo:rustc-link-lib=db2app64"); // the "-l" flag
 }
 
