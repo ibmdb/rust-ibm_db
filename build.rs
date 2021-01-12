0d7ae196ca573bf7c80dd67f1d1eb9de9c7799de
Binit Kumar <bkumar@rocketsoftware.com>
2021-01-12 12:20:44 +0530

    Removed hardcoding

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 
 fn main() {
-    println!("cargo:rustc-link-search=/home/clidriver/lib"); // the "-L" flag
+    let project_dir = env::var("IBM_DB_HOME").unwrap();
+    println!("cargo:rustc-link-search={}/{}",project_dir,"lib"); // the "-L" flag
     println!("cargo:rustc-link-lib=db2"); // the "-l" flag
+
 }
 
