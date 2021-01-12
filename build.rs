675e8cb5e13b27ad699c8933f2c345307b617031
Binit Kumar <bkumar@rocketsoftware.com>
2021-01-12 12:46:18 +0530

    LINUX compatible

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 use std::env;
 fn main() {
     let project_dir = env::var("IBM_DB_HOME").unwrap();
-    println!("cargo:rustc-link-search={}/{}",project_dir,"/clidriver/lib"); // the "-L" flag
-    println!("cargo:rustc-link-lib=db2app64"); // the "-l" flag
-
+    println!("cargo:rustc-link-search={}/{}",project_dir,"/lib"); // the "-L" flag
+    println!("cargo:rustc-link-lib=db2"); // the "-l" flag
+//Change this to db2app64 for Windows.
+    //println!("cargo:rustc-link-lib=db2app64"); // the "-l" flag
 }
 
