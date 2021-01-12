b520bc5103ec7538c3d1a2acd497872d95d94063
Binit Kumar <bkumar@rocketsoftware.com>
2021-01-12 12:42:23 +0530

    New build

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
-
+use std::env;
 fn main() {
     let project_dir = env::var("IBM_DB_HOME").unwrap();
-    println!("cargo:rustc-link-search={}/{}",project_dir,"lib"); // the "-L" flag
-    println!("cargo:rustc-link-lib=db2"); // the "-l" flag
+    println!("cargo:rustc-link-search={}/{}",project_dir,"/clidriver/lib"); // the "-L" flag
+    println!("cargo:rustc-link-lib=db2app64"); // the "-l" flag
 
 }
 
