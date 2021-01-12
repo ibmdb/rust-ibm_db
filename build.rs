d3944b8f28649384585ac1354fa6c2af59da174a
Binit Kumar <bkumar@rocketsoftware.com>
2021-01-12 19:21:20 +0530

    MACOS related changes

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
+#![allow(unused_assignments)]
 use std::env;
+
 fn main() {
-    let project_dir = env::var("IBM_DB_HOME").unwrap();
-    println!("cargo:rustc-link-search={}/{}",project_dir,"/lib"); // the "-L" flag
+    let env_var_not_present = env::var("IBM_DB_HOME").is_err();
+	let mut project_dir = String::new();
+	if env_var_not_present {
+		project_dir = ".".parse().unwrap();
+	}else{
+		project_dir = env::var("IBM_DB_HOME").unwrap();
+	}
+    println!("cargo:rustc-link-search={}/{}",project_dir,"lib"); // the "-L" flag
     println!("cargo:rustc-link-lib=db2"); // the "-l" flag
-//Change this to db2app64 for Windows.
+    //Change this to db2app64 for Windows.
     //println!("cargo:rustc-link-lib=db2app64"); // the "-l" flag
 }
 
