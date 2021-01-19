47a11be4d22b42bfd4f89ecf0dc5651b1df2a0d6
Binit Kumar <bkumar@rocketsoftware.com>
2021-01-19 11:48:05 +0530

    Cleaning code

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,48 @@
-
-
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals, dead_code, improper_ctypes)]
 
 use ::ibm_db::SQLConnect;
-use ibm_db::{SQLAllocHandle, SQL_HANDLE_DBC, SQL_HANDLE_ENV, SQL_SUCCESS, SQLDisconnect, SQLFreeHandle, SQLGetInfo, SQLSMALLINT, SQL_DATA_SOURCE_NAME, SQLGetFunctions, SQL_API_SQLGETINFO, SQLUSMALLINT, SQL_TRUE, SQLExecDirect, SQL_NTS, SQLCHAR, SQLSetConnectAttr, SQL_ATTR_AUTOCOMMIT, SQL_AUTOCOMMIT_ON, SQLPOINTER, SQL_HANDLE_STMT, SQL_NO_DATA_FOUND, SQLFreeStmt, SQL_UNBIND, SQL_RESET_PARAMS, SQL_CLOSE};
-use std::ffi::CStr;
-
+use ibm_db::{SQLAllocHandle, SQL_HANDLE_DBC, SQL_HANDLE_ENV, SQL_SUCCESS, SQLFreeHandle, SQLSMALLINT, SQLExecDirect, SQL_NTS, SQLCHAR, SQLSetConnectAttr, SQL_ATTR_AUTOCOMMIT, SQL_AUTOCOMMIT_ON, SQLPOINTER, SQL_HANDLE_STMT, SQL_NO_DATA_FOUND, SQLFreeStmt, SQL_UNBIND, SQL_RESET_PARAMS, SQL_CLOSE};
 
 fn main() {
-    connect(
-        "dashdb".parse().unwrap(),
-        "admin".parse().unwrap(),
-        "admin".parse().unwrap()
-    )
+    let dsn = "dashdb";
+    let uid = "admin";
+    let pwd = "admin";
+    let conn = connect(
+        dsn.parse().unwrap(),
+        uid.parse().unwrap(),
+        pwd.parse().unwrap()
+    );
+    unsafe{
+        if conn == 0{
+            println!("Connection failed...");
+            std::process::exit(-1);
+        }
+        dml(conn);
+        closeConnection(conn);
+    }
 }
 
-fn connect(mut dsn : String, mut uid: String, mut pwd: String){
-    unsafe{
+fn connect(mut dsn : String, mut uid: String, mut pwd: String) -> i32 {
+    unsafe {
         let mut hdbc = 0;
         let mut cliRC;
         let mut out = 0;
         cliRC = SQLAllocHandle(SQL_HANDLE_ENV as i16,
                                0,
-                               &mut out) ;
-        if cliRC!= SQL_SUCCESS as i16 {
-            println!("--ERROR while allocating the environment handle. Status: {}",cliRC);
-            return;
+                               &mut out);
+        if cliRC != SQL_SUCCESS as i16 {
+            println!("--ERROR while allocating the environment handle. Status: {}", cliRC);
+            return 0;
         }
 
         cliRC = SQLAllocHandle(SQL_HANDLE_DBC as i16,
                                out,
                                &mut hdbc);
-        if cliRC!= SQL_SUCCESS as i16 {
-            println!("--ERROR while getting hdbc. Status: {}",cliRC);
-            return;
+        if cliRC != SQL_SUCCESS as i16 {
+            println!("--ERROR while getting hdbc. Status: {}", cliRC);
+            return 0;
         }
-        println!("Connecting to database: {} .........",dsn);
+        println!("Connecting to database: {} .........", dsn);
         SQLConnect(
             hdbc
             ,
@@ -51,93 +58,61 @@ fn connect(mut dsn : String, mut uid: String, mut pwd: String){
             ,
             pwd.len() as i16
         );
-        println!("Connected Successfully to database: {}",dsn);
 
-        let mut hstmt= 0;
+        SQLSetConnectAttr(hdbc, SQL_ATTR_AUTOCOMMIT as i32, SQL_AUTOCOMMIT_ON as SQLPOINTER, SQL_NTS);
 
-        SQLSetConnectAttr(hdbc,SQL_ATTR_AUTOCOMMIT as i32,SQL_AUTOCOMMIT_ON as SQLPOINTER,SQL_NTS);
+        let mut hstmt = 0;
 
-        println!("Allocating Statement Handle");
         cliRC = SQLAllocHandle(SQL_HANDLE_STMT as SQLSMALLINT,
                                hdbc,
                                &mut hstmt);
-        if cliRC!= SQL_SUCCESS as i16 {
-            println!("--ERROR while getting hdbc. Status: {}",cliRC);
-            return;
+        if cliRC != SQL_SUCCESS as i16 {
+            println!("--ERROR while getting statement. Status: {}", cliRC);
+            return 0;
         }
-
-        println!("Dropping table if it exists.....");
-        let mut query = "DROP TABLE TEST";
-        let mut stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
-        cliRC = SQLExecDirect(hstmt,stmt,query.as_bytes().len() as i32);
-        println!("Dropping Table Result: {}",cliRC);
-
-        println!("Creating table.....");
-        query = "create table test(Col3 VARCHAR(7))";
-        stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
-        cliRC = SQLExecDirect(hstmt,stmt,query.as_bytes().len() as i32);
-        println!("Creating Table Result: {}",cliRC);
-
-        println!("Inserting Data.....");
-        query = "INSERT INTO TEST VALUES ('Binit')";
-        stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
-
-        cliRC = SQLExecDirect(hstmt,stmt,query.as_bytes().len() as i32);
-        println!("Inserting Data Result: {}",cliRC);
-
-        println!("Fetching Data.....");
-        query = "SELECT * FROM TEST";
-        stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
-        cliRC = SQLExecDirect(hstmt,stmt,query.as_bytes().len() as i32);
-        if cliRC == SQL_NO_DATA_FOUND as i16{
-            println!("No Data in Table");
-        } else{
-            println!("Fetching Data Result: {}",cliRC);
-        }
-
-        //After Select, Handle is missing so recreating the same.
-        println!("Allocating Statement Handle");
-        cliRC = SQLAllocHandle(SQL_HANDLE_STMT as SQLSMALLINT,
-                               hdbc,
-                               &mut hstmt);
-        if cliRC!= SQL_SUCCESS as i16 {
-            println!("--ERROR while getting hdbc. Status: {}",cliRC);
-            return;
-        }
-
-
-        println!("Dropping table.....");
-        query = "DROP TABLE TEST";
-        stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
-        cliRC = SQLExecDirect(hstmt,stmt,query.as_bytes().len() as i32);
-        println!("Dropping Table Result: {}",cliRC);
-
-
-        println!("Freeing Statement Handle");
-        SQLFreeHandle(SQL_HANDLE_STMT as i16,hstmt);
-        SQLFreeStmt(hstmt,SQL_UNBIND as u16);
-        SQLFreeStmt(hstmt,SQL_RESET_PARAMS as u16);
-        SQLFreeStmt(hstmt,SQL_CLOSE as u16);
-
-        let dbInfoBuf = core::ptr::null_mut();
-        let outlen:*mut SQLSMALLINT = core::ptr::null_mut();
-        let supported:*mut SQLUSMALLINT = core::ptr::null_mut();
-        /* check to see if SQLGetInfo() is supported */
-        SQLGetFunctions(hdbc, SQL_API_SQLGETINFO as u16,
-                                supported);
-        if supported == SQL_TRUE as *mut u16
-        {
-            SQLGetInfo(hdbc, SQL_DATA_SOURCE_NAME as u16,
-                       dbInfoBuf, 255, outlen);
-            let cstr = CStr::from_ptr(dbInfoBuf as *const _).to_string_lossy();
-            println!("dbInfoBuf has value: {}", !dbInfoBuf.is_null());
-            println!("DSN name: {}", cstr);
-        }
-        println!("Disconnecting database: {}",dsn);
-        SQLDisconnect(hdbc);
-        println!("Disconnected Successfully from database: {}",dsn);
-        println!("Freeing connection Handle");
-        SQLFreeHandle(SQL_HANDLE_DBC as i16,hdbc);
+        println!("Connected Successfully to database: {}", dsn);
+        return hstmt;
     }
+}
+
+pub unsafe fn dml(conn: i32){
+    let mut cliRC;
+
+    println!("Dropping table if it exists.....");
+    let mut query = "DROP TABLE TEST";
+    let mut stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
+    cliRC = SQLExecDirect(conn, stmt, query.as_bytes().len() as i32);
+    println!("Dropping Table Result: {}",cliRC);
+
+    println!("Creating table.....");
+    query = "create table test(Col3 VARCHAR(7))";
+    stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
+    cliRC = SQLExecDirect(conn, stmt, query.as_bytes().len() as i32);
+    println!("Creating Table Result: {}",cliRC);
+
+    println!("Inserting Data.....");
+    query = "INSERT INTO TEST VALUES ('Binit')";
+    stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
+
+    cliRC = SQLExecDirect(conn, stmt, query.as_bytes().len() as i32);
+    println!("Inserting Data Result: {}",cliRC);
+
+    println!("Fetching Data.....");
+    query = "SELECT * FROM TEST";
+    stmt = query.as_bytes().as_ptr() as *mut SQLCHAR;
+    cliRC = SQLExecDirect(conn, stmt, query.as_bytes().len() as i32);
+    if cliRC == SQL_NO_DATA_FOUND as i16{
+        println!("No Data in Table");
+    } else{
+        println!("Fetching Data Result: {}",cliRC);
+    }
+}
+pub unsafe fn closeConnection(conn:i32){
+    println!("Disconnecting ...");
+    SQLFreeHandle(SQL_HANDLE_STMT as i16, conn);
+    SQLFreeStmt(conn, SQL_UNBIND as u16);
+    SQLFreeStmt(conn, SQL_RESET_PARAMS as u16);
+    SQLFreeStmt(conn, SQL_CLOSE as u16);
+    println!("Disconnected Successfully.");
 
 }
